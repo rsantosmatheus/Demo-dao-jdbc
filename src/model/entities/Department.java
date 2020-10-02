7f9dd5b79cb3427c55484f2b000a34b0708d5f11
Matheus <rsantos.matheus@outlook.com>
2020-10-02 16:05:32 -0300

    Seller entities class

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -5,28 +5,34 @@ import java.io.Serializable;
 public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
-	
+
 	public Department() {
-		}
+	}
+
 	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -34,6 +40,7 @@ public class Department implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -50,6 +57,7 @@ public class Department implements Serializable {
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
